e53a5ad26d4dcf5828e0f59826891cd5f1f112bf
Adan Santos <santos.adaan@gmail.com>
2018-04-06 17:00:17 -0300

    aa

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -13,33 +13,118 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
     this.generalImbuements = [
-      { name: 'Scorch', description: 'Converte 10% do dano fisico em dano de fogo.'},
-      { name: 'Venom', description: 'Converte 10% do dano fisico em dano de terra.'},
-      { name: 'Frost', description: 'Converte 10% do dano fisico em dano de gelo.'},
-      { name: 'Electrify', description: 'Converte 10% do dano fisico em dano de energia.'},
-      { name: 'Reap', description: 'Converte 10% do dano fisico em dano de morte.'},
-
-      { name: 'Lich Shroud', description: 'Reduz o dano de morte em 3%.'},
-      { name: 'Snake Skin', description: 'Reduz o dano de morte em 3%.'},
-      { name: 'Dragon Hide', description: 'Reduz o dano de fogo em 3%.'},
-      { name: 'Quara Scale', description: 'Reduz o dano de gelo em 3%.'},
-      { name: 'Cloud Fabric', description: 'Reduz o dano de energia em 3%.'},
-      { name: 'Demon Presence', description: 'Reduz o dano de sagrado em 3%.'},
-
-      { name: 'Swiftness', description: 'Aumenta a velocidade em 10.'},
-      { name: 'Chop', description: 'Aumenta o Skill de Axe em 1.'},
-      { name: 'Slash', description: 'Aumenta o Skill de Sword em 1.'},
-      { name: 'Bash', description: 'Aumenta o Skill de Club em 1.'},
-      { name: 'Precision', description: 'Aumenta o Skill de Distance em 1.'},
-      { name: 'Blockade', description: 'Aumenta o Skill de Shield em 1.'},
-      { name: 'Epiphany', description: 'Aumenta o Skill de Magic Level em 1.'},
-      { name: 'Featherweight', description: 'Aumenta a capacidade total em 3%.'},
+      { 
+        name: 'Scorch',
+        description: 'Converte % do dano fisico em dano de fogo.',
+        basic: 'fiery heart',
+        intricate: 'green dragon scale',
+        powerfull: 'demon horn'
+      },
+      { 
+        name: 'Venom',
+        description: 'Converte % do dano fisico em dano de terra.',
+        basic: 'swamp grass',
+        intricate: 'poisonous slime',
+        powerfull: 'slime heart'
+      },
+      { 
+        name: 'Frost', 
+        description: 'Converte % do dano fisico em dano de gelo.',
+        basic: 'frosty heart',
+        intricate: 'seacrest hair',
+        powerfull: 'polar bear paw'
+      },
+      { 
+        name: 'Electrify', 
+        description: 'Converte % do dano fisico em dano de energia.',
+        basic: 'rorc feathers',
+        intricate: 'peacock feather fan',
+        powerfull: 'energy vein'
+      },
+      { 
+        name: 'Reap', 
+        description: 'Converte % do dano fisico em dano de morte.'
+      },
+      { 
+        name: 'Lich Shroud', 
+        description: 'Reduz uma % do dano de morte.'
+      },
+      { 
+        name: 'Snake Skin', 
+        description: 'Reduz uma % do dano de morte.'
+      },
+      { 
+        name: 'Dragon Hide', 
+        description: 'Reduz uma % do dano de fogo.'
+      },
+      { 
+        name: 'Quara Scale', 
+        description: 'Reduz uma % do dano de gelo.'
+      },
+      { 
+        name: 'Cloud Fabric', 
+        description: 'Reduz uma % do dano de energia.'
+      },
+      { 
+        name: 'Demon Presence', 
+        description: 'Reduz uma % do dano de sagrado.'
+      },
+      { 
+        name: 'Swiftness', 
+        description: 'Aumenta a velocidade.'
+      },
+      { 
+        name: 'Chop', 
+        description: 'Aumenta o Skill de Axe.'
+      },
+      { 
+        name: 'Slash', 
+        description: 'Aumenta o Skill de Sword.'
+      },
+      { 
+        name: 'Bash', 
+        description: 'Aumenta o Skill de Club.'
+      },
+      { 
+        name: 'Precision', 
+        description: 'Aumenta o Skill de Distance.'
+      },
+      { 
+        name: 'Blockade', 
+        description: 'Aumenta o Skill de Shield.'
+      },
+      { 
+        name: 'Epiphany', 
+        description: 'Aumenta o Skill de Magic Level.'
+      },
+      { 
+        name: 'Featherweight', 
+        description: 'Aumenta a capacidade total.'
+      },
     ]
 
     this.tokenImbuements = [
-      { name: 'Vampirism', description: 'Converte 5% do dano causado em vida (probabilidade de 100%).'},
-      { name: 'Void', description: 'Converte 3% do dano causado em mana (probabilidade de 100%).'},
-      { name: 'Strike', description: 'Aumenta o dano crítico em 15% (probabilidade de 10%).'},
+      { 
+        name: 'Vampirism', 
+        description: 'Converte % do dano causado em vida (probabilidade de 100%).',
+        basic: 'vampire teeth',
+        intricate: 'bloody pincers',
+        powerfull: 'piece of dead brain'
+      },
+      { 
+        name: 'Void', 
+        description: 'Converte % do dano causado em mana (probabilidade de 100%).',
+        basic: 'rope belt',
+        intricate: 'silencer claws',
+        powerfull: 'some grimeleech wings'
+      },
+      { 
+        name: 'Strike', 
+        description: 'Aumenta a % do dano crítico (probabilidade de 10%).',
+        basic: 'protective charm',
+        intricate: 'sabretooth',
+        powerfull: 'vexclaw talon'
+      },
     ]
   }
 
